8a959a6c8486dcd46ebb18b5be410ad7e72be42e
Mallikarjun <tirumala.mreddy@gmail.com>
2021-08-27 11:44:24 +0530

    updated main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+		System.out.println("Sample1");
 	System.out.println("Sample");
+	   }
+
+	   public static void min(){
+		   System.out.println("min fuction");
 	   }
 }
