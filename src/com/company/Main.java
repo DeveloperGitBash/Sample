5e5c9272d841ebda991551f9110f2ae8c347859d
Mallikarjun <tirumala.mreddy@gmail.com>
2021-08-27 13:19:11 +0530

    updated main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
 		System.out.println("Sample1");
 		System.out.println("Sample0");
-	    System.out.println("Sample1");
+	    System.out.println("Sample2");
 	   }
 
 	   public static void min(){
 		   System.out.println("min fuction");
 	   }
 
-	public static max(){
-		System.out.println("max function");
+	public static void max(){
+		System.out.println("max method");
 	}
 }
