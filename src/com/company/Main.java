ba6ea725ad3332853812e6ff0d026f0c9351fb5d
DeveloperGitBash <55375940+DeveloperGitBash@users.noreply.github.com>
2021-08-27 11:42:34 +0530

    Update Main.java
    add changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Sample");
+	System.out.println("Sample0");
+	    System.out.println("Sample1");
 	   }
+	
+	public static max(){
+		System.out.println("max function");
+	}
 }
